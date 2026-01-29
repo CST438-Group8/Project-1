6e14da4a92e4759023318f99db5fb94bcf252e9c
JordanLagura <alagura@csumb.edu>
2026-01-28 16:27:04 -0800

    Updated - activity_landing.xml - activity_login.xml
    Added delay on text in landing screen
    Fixed centering issues on login screen

--- app/src/main/java/com/example/project1_triviagame/LandingActivity.kt
+++ app/src/main/java/com/example/project1_triviagame/LandingActivity.kt
@@ -3,16 +3,31 @@ package com.example.project1_triviagame
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import android.content.Intent
+import android.os.Handler
+import android.os.Looper
+import android.view.View
+import android.view.animation.DecelerateInterpolator
 
-class LandingActivity : AppCompatActivity(){
+class LandingActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_landing)
 
-        findViewById<android.view.View>(android.R.id.content).setOnClickListener {
-            val intent = android.content.Intent(this, LoginActivity::class.java)
-            startActivity(intent)
+        val tapText = findViewById<View>(R.id.tapText)
+
+        // delay 1 second - https://android.googlesource.com/platform/frameworks/support/+/5d1be99/v4/java/android/support/v4/app/FragmentManager.java
+        Handler(Looper.getMainLooper()).postDelayed({
+            tapText.animate()
+                .alpha(1f)
+                .setDuration(500)
+                .setInterpolator(DecelerateInterpolator())
+                .start()
+        }, 1000)
+
+
+        findViewById<View>(android.R.id.content).setOnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java))
         }
     }
 }
