8eb84b5b8dfd0939414dd7036005c214df43cd83
Raphael Berjaoui <rhberjaoui.com>
2026-02-04 16:41:59 -0800

    Made the screen to select difficulty. -Renamed HomeScreen.kt to DifficultyScreen.kt -Put a new font in Theme.kt -Included the font in bold and regular in res file -Implements a Easy, Medium, Hard button DifficultyScreen.kt -Left comments to point out where DifficultyScreen.ktis taking from theme

--- app/src/main/java/com/example/project1_triviagame/ui/theme/Theme.kt
+++ app/src/main/java/com/example/project1_triviagame/ui/theme/Theme.kt
@@ -1,25 +1,13 @@
 package com.example.project1_triviagame.ui.theme
 
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Color
-
-private val AppDarkColorScheme = darkColorScheme(
-    primary = Color(0xFF8C817A),
-    onPrimary = Color.White,
-    background = Color(0xFF0B2A4A),
-    surface = Color(0xFF0B2A4A),
-    onBackground = Color.White,
-    onSurface = Color.White
-)
 
 @Composable
 fun Project1_TriviaGameTheme(
     content: @Composable () -> Unit
 ) {
-    MaterialTheme(
-        colorScheme = AppDarkColorScheme,
-        content = content
-    )
+    MaterialTheme {
+        content()
+    }
 }
