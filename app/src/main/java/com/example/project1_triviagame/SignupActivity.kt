d1d7ec963f1084e7cddbca025267cf20f1d49460
JordanLagura <alagura@csumb.edu>
2026-02-04 20:56:50 -0800

    Created SignupActivity.kt - xml views - sign up login button - return to login - room logic
    fixed issues in xmls

--- app/src/main/java/com/example/project1_triviagame/SignupActivity.kt
+++ app/src/main/java/com/example/project1_triviagame/SignupActivity.kt
@@ -1,22 +1,49 @@
 package com.example.project1_triviagame
 
-import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
-class SignupActivity : AppCompatActivity() {
+class SignUpActivity : AppCompatActivity() {
+
+    private lateinit var onClickListener: () -> Unit
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
 
-        val loginText = findViewById<TextView>(R.id.logInText)
+        // xml views
+        val usernameInput = findViewById<EditText>(R.id.usernameInput)
+        val passwordInput = findViewById<EditText>(R.id.passwordInput)
+        val confirmPasswordInput = findViewById<EditText>(R.id.confirmPasswordInput)
+        val signUpButton = findViewById<Button>(R.id.signUpButton)
+        val logInText = findViewById<TextView>(R.id.logInText)
 
-        loginText.setOnClickListener {
-            val intent = Intent(this, LoginActivity::class.java)
-            startActivity(intent)
-            finish() // no stacking screens
+        // Sign up button logic
+        signUpButton.setOnClickListener {
+            val username = usernameInput.text.toString()
+            val password = passwordInput.text.toString()
+            val confirmPassword = confirmPasswordInput.text.toString()
+
+            if (password == confirmPassword) {
+                signUp(username, password)
+            } else {
+                Toast.makeText(this, "Passwords do not match", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        // login screen
+        logInText.setOnClickListener {
+            finish()
         }
     }
+
+    // sign up logic room
+    private fun signUp(username: String, password: String) {
+        TODO("Not yet implemented")
+    }
 }
+
